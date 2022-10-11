8770d77c04c978d8028f57636a163e23990b2b9e
Akarui <115368809+Akarui-Nobi@users.noreply.github.com>
2022-10-11 02:03:13 +0200

    Add files via upload

---
+++ Collision Detection/circle.js
@@ -0,0 +1,15 @@
+const circle1 = {x: 10, y: 10, radius: 300};
+const circle2 = {x: 500, y: 500, radius: 150};
+
+let dx = circle2.x - circle1.x;
+let dy = circle2.y - circle1.y;
+let distance = Math.sqrt(dx * dx + dy * dy);
+let sumOfRadius = circle1.radius + circle2.radius;
+
+if (distance < sumOfRadius){
+    // circles collide
+} else if (distance === sumOfRadius){
+    // circles are touching
+} else if (distance > sumOfRadius){
+    // no collision
+}
